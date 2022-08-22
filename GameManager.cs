58fe7b3069658cffca41a5d0ed3fbd373e4f457b
Soluhh <111203670+Soluhh@users.noreply.github.com>
2022-08-22 12:27:46 -0700

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Score Elements")]
+    public int score;
+    public int highscore;
+    public Text scoreText;
+    public Text highscoreText;
+
+    [Header("Game Over")]
+    public GameObject gameOverPanel;
+    public Text gameOverPanelScoreText;
+    public Text gameOverPanelHighscoreText;
+
+    private void Awake()
+    {
+        gameOverPanel.SetActive(false);
+        highscore = PlayerPrefs.GetInt("Highscore");
+        highscoreText.text = "Best: " + highscore;
+    }
+
+    private void GetHighScore()
+    {
+        highscore = PlayerPrefs.GetInt("Highscore");
+        highscoreText.text = "Best: " + highscore;
+    }
+
+    public void IncreaseScore(int points)
+    {
+        score += points;
+        scoreText.text = score.ToString();
+
+        if(score > highscore)
+        {
+            PlayerPrefs.SetInt("Highscore", score);
+            highscoreText.text = "Best: " + score.ToString();
+        }
+    }
+
+    public void OnBombHit()
+    {
+        Time.timeScale = 0;
+
+        gameOverPanelScoreText.text = "Score: " + score.ToString();
+        gameOverPanelHighscoreText.text = "Highscore: " + highscore.ToString();
+        gameOverPanel.SetActive(true);
+
+        Debug.Log("Bomb hit!");
+    }
+
+    public void RestartGame()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+
+        gameOverPanel.SetActive(false);
+
+        foreach(GameObject g in GameObject.FindGameObjectsWithTag("Interactable"))
+        {
+            Destroy(g);
+        }
+
+        Time.timeScale = 1;
+    }
+}
