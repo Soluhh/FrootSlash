166816363b9d3f686ed2d153ecef55431699c20e
Soluhh <111203670+Soluhh@users.noreply.github.com>
2022-08-22 13:03:00 -0700

    Add files via upload

---
+++ Scripts/BombScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Blade b = collision.GetComponent<Blade>();
+
+        if(!b)
+        {
+            return;
+        }
+
+        FindObjectOfType<GameManager>().OnBombHit();
+    }
+}
